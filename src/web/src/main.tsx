5baa3e81686e57fdb2989fbb195586e7170b0bd4
Wesley-dSA <wesley.aragao1331@gmail.com>
2025-04-10 20:43:41 -0300

    Praticamente Finalizado

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -2,13 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Login from './pages/login.tsx'
+import LoginPage from './pages/login.tsx'
+import Initial from './pages/initial.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path="/initial" element={<Initial />} />
     </Routes>
   </BrowserRouter>,
 )
--- src/web/src/pages/login.tsx
+++ src/web/src/pages/login.tsx
@@ -1,48 +1,97 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const Login: React.FC = () => {
-  const [username, setUsername] = useState<string>('');
+function LoginPage() {
+  const [username, setNome] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const handleLogin = (event: React.FormEvent) => {
-    event.preventDefault();
-    
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log("Form submitted");
 
-  };
+    const payload = {
+        nome: username,
+        senha: password
+    };
+
+    try {
+        const response = await fetch("http://localhost:5173/login", {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const textResponse = await response.text();
+
+        console.log("Resposta recebida:", textResponse);
+
+
+        if (!response.ok) {
+            console.log("Erro ao tentar logar. Verifique suas credenciais.");
+        }
+
+        setTimeout(() => {
+            window.location.href = "/initial";
+        }, 1000);
+
+    } catch (err: any) {
+        console.error(err);
+    }
+}
 
   return (
-        <div className=' bg-green-100 w-72 h-90 justify-self-center'>
-        <h2 className='text-3xl pt-10 pb-12'>Login</h2>
-        <form onSubmit={handleLogin}>
-            <div>
-            <input
-            className='mt-5 mb-5 bg-green-50 rounded-sm indent-3'
-                type="text"
-                id="username"
-                placeholder='Usuário'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            </div>
-            <div>
-            <input
-                className='mt-5 mb-10 bg-green-50 rounded-sm indent-3'
-                type="password"
-                id="password"
-                placeholder='Senha'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            </div>
-            <button type="submit" 
-            className="bg-emerald-600 shadow-lg rounded-sm w-20 h-12 
-            items-center"
-            >Login</button>
-        </form>
-        </div>
-  );
-};
+    <div className="flex items-center justify-center min-h-screen bg-green-50 relative">
+    <button
+        onClick={() => window.location.href = "/"}
+        className="cursor-pointer absolute right-4 top-4 bg-green-800 text-white p-2 rounded-md hover:bg-green-700"
+    >
+        Voltar
+    </button>
 
-export default Login;
+    <div className="w-full h-100 max-w-md p-8 space-y-6 bg-green-100 rounded-2xl shadow-lg shadow-black/30">
+        <h2 className="text-3xl font-bold text-center text-green-800">Login</h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="p-2">
+                <label className="text-left block text-lg font-medium text-green-700 ">Nome</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setNome(e.target.value)}
+                    name="nome"
+                    required
+                    className="bg-white w-full p-2 mt-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-black/75 shadow-sm shadow-black/50"
+                    placeholder="Insira seu Nome"
+                />
+            </div>
+            <div className="p-2">
+                <label className="text-left block text-lg font-medium text-green-700">Senha</label>
+                <div className="relative">
+                    <input
+                        type={password ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        name="password"
+                        required
+                        className="bg-white w-full p-2 mt-1 rounded-lg focus:outline-none focus:ring-2 focus:ring-black/75 shadow-sm shadow-black/50"
+                        placeholder="Insira sua Senha"
+                    />
+                    
+                </div>
+            </div>
+            <div className="flex flex-col items-center p-2">
+                <button
+                    type="submit"
+                    className="cursor-pointer w-40 p-2 font-semibold text-white bg-green-500 rounded-lg hover:bg-green-600 shadow-sm shadow-black/50
+                    duration-500 ease-in-out hover:w-42 hover:h-10"
+                    onClick={() => handleSubmit}
+                >
+                    Entrar
+                </button>
+            </div>
+        </form>
+    </div>
+
+    
+  </div>
+    )}
+
+export default LoginPage;
