42b66a67d673de57fee600cee6f46e004b7cb134
Wesley dos Santos Aragão <125709400+Wesley-dSA@users.noreply.github.com>
2025-04-19 21:53:29 -0300

    web: login page (#2)
    Co-authored-by: Victor Tedesco <victor.augusto.tedesco@gmail.com>
    Co-authored-by: WESLEY  DOS SANTOS ARAGAO <0220482312009@fatectq.local>

--- src/web/src/main.tsx
+++ src/web/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { LoginPage } from './pages/LoginPage.tsx'
+import { NotFoundPage } from './pages/NotFoundPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>,
 )
